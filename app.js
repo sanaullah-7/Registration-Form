7dc498364458dfa8d532aab3b56a629c68c9ff26
sanaullah-7 <sanaullahyousafzai01@gmail.com>
2025-05-22 12:04:41 -0700

    image insert and delete

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ const Age = e.target[5].value;
 const DOB = e.target[6].value;
 const Gender = e.target[7].value;
 const Result = document.createElement("div")
+const Photo = document.getElementById("photo").files[0];//.files[0] means: get the first file the user selected.
+//Generate Image
+// Initialize image URL
+// Ek khaali string banao jisme image ka link store hoga
+let imageURL = "";
+// If a file is selected, create a temporary URL to preview it // Preview ka matlab: bina upload kiye image ko dikhana bina server par bheje.
+  if (Photo) {
+    imageURL = URL.createObjectURL(Photo);
+  } 
+  //e.g Tum Facebook ya WhatsApp pe image select karte ho aur wo turant dikh jaati hai —
+//upload hone se pehle — bas waisa hi hota hai preview! 
+   
 console.log(Name)
 e.preventDefault();//it stops the form from submitting to the reloading of page.
 e.target.reset();//This clears the form inputs & resets the form to its original state.
@@ -28,37 +40,39 @@ e.target.reset();//This clears the form inputs & resets the form to its original
  emailError.textContent = "";
  PasswordError.textContent = "";
  ageError.textContent = "";
-//Agar aap innerHTML ka use karte hain, toh agar aap error messages mein HTML tags jaise <b>, <i>, <div>, etc. daal rahe ho, toh wo tags render ho jayenge.
+//Agar aap textcontent ka use karte hain, toh agar aap error messages mein HTML tags jaise <b>, <i>, <div>, etc. daal rahe ho, toh wo tags render ho jayenge.
 
  // Simple Checks
  let isValid = true;
 
  if (Name=== "" || Name.length < 2) {
-   nameError.textContent = "Please enter a valid name.";
+   nameError.innerHTML = "<strong>Please enter a valid name.";
    isValid = false;//es ka maylab Yeha ha k jab isValid false ho jayega(let isValid = true k bat hurahe ha) aur form ko submit nahi karne diya jayega.
  }
  if (Pnumber == "" || Pnumber.length > 11  ) {
-   phoneError.textContent = "Please enter a 11-Digit .";
+   phoneError.innerHTML = "<strong>Please enter a 11-Digit .";
    isValid = false;
  }
  if (!Email.includes("@") || !Email.includes(".")) {
-   emailError.textContent = "Please enter a valid email.";
+   emailError.innerHTML = "<strong>Please enter a valid email.";
    isValid = false;
  }
  if (Password === "" || Password.length <= 7) {
-   PasswordError.textContent = "Please enter at least 8 leaters.";
+   PasswordError.innerHTML = "<strong>Please enter at least 8 leaters.";
    isValid = false;
  }
 
  if (Age === "" || isNaN(Age) || Number(Age) <= 0) {
-   ageError.textContent = "Please enter a valid age.";
+   ageError.innerHTML = "<strong>Please enter a valid age.</strong>";
    isValid = false;
    
  }
 //  Stop if there are any errors
- if (isValid === false) return;
+ if (isValid === false) 
  alert("Form Submitted Successfully!")
-Result.innerHTML=`<b>ID : </b>A${UserID}<br>
+Result.innerHTML=`
+<img src="${imageURL}" width="100" height="100" style="object-fit:cover, marginTop:4px" /><br> 
+<b>ID : </b>A${UserID}<br>
 <b>Name : </b>${Name}<br>
 <b>Phone NO : </b>${Pnumber}<br>
 <b>Email : </b>${Email}<br>
@@ -66,10 +80,17 @@ Result.innerHTML=`<b>ID : </b>A${UserID}<br>
 <b>Address : </b>${Address}<br>
 <b>Age : </b>${Age}<br>
 <b>DOB : </b>${DOB}<br>
-<b>Gender : </b>${Gender}<br>`;
+<b>Gender : </b>${Gender}<br>
+<button class="editBtn">Edit</button>
+<button class="deleteBtn">Delete</button>`;
 document.getElementById("result").append(Result);//Ye keh raha hai:“Jo result wala div hai us ke andar Result ko neechay add kar do
 //append “Add something at the end of an element.”
 UserID++;
+ // Delete functionality
+  Result.querySelector(".deleteBtn").addEventListener("click", () => {
+    Result.remove();
+  });
+  
 
 });
 
